9dfa236270735fbe53ec58c69a64367d481fb26e
Elder <elucas-2015337@kinal.edu.gt>
2021-02-25 18:48:27 -0600

    Cambios de logo, versión y linea e conexión

--- www/js/services/ValidacionDeLicenciaServicio.js
+++ www/js/services/ValidacionDeLicenciaServicio.js
@@ -50,8 +50,8 @@ function getConf(callback) {
     
             reader.onloadend = function(e) {
                 if (this.result == '') {
-                    writeLog(`{"url": "http://52.149.161.64:8085/"}`)
-                    callback({"url": "http://52.149.161.64:8085/"});
+                    writeLog(`{"url": "http://52.188.206.178:8083/"}`)
+                    callback({"url": "http://52.188.206.178:8083/"});
                 }else {
                     callback(JSON.parse(this.result));
                 }
