934bc4907c301225d25b27fee74567b6282fda8a
procesos-gildardo <gildardo.a@procesoseficientes.com>
2021-04-12 16:26:39 -0600

    Se quito duplicado de producto al finalizar ruta

--- www/js/services/FinRutaServicio.js
+++ www/js/services/FinRutaServicio.js
@@ -295,6 +295,7 @@ function ObtenerProductosDeOrdenesDeVenta(callback, errCallBack) {
       sql += " d.SKU, s.SKU_NAME, SUM(d.QTY) QTY, SUM(d.TOTAL_LINE) TOTAL";
       sql += " FROM SALES_ORDER_DETAIL d INNER JOIN SKU_PRESALE s";
       sql += " ON d.SKU = s.SKU";
+      sql += " AND s.WAREHOUSE = '" + localStorage.getItem("PRESALE_WHS") + "'";
       sql += " GROUP BY s.SKU,s.SKU_NAME";
 
       tx.executeSql(
