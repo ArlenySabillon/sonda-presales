5cb31967d51d3ab383ecc3febd718084822ff5fb
Elder <elucas-2015337@kinal.edu.gt>
2021-07-08 15:58:56 -0600

    descuento en blanco, microencuestas

--- www/js/globalutils.js
+++ www/js/globalutils.js
@@ -94,7 +94,7 @@ var default_image;
 var SondaServerURL = "";
 
 var currentBranch = "cendalzaRoute";
-var SondaVersion = "2021.06.30";
+var SondaVersion = "2021.07.07";
 var SondaServerOptions = {
     reconnect: true,
     "max reconnection attempts": 60000
